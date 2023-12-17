a7c25606038f28401f124019044953b76efd8394
Matias <m.kaliroff@gmail.com>
2023-12-17 14:41:36 +0200

    queue

---
+++ src/queue/my_queue.hpp
@@ -0,0 +1,39 @@
+#ifndef __MY_QUEUE_HPP__
+#define __MY_QUEUE_HPP__
+
+#include <cstddef>  // size_t
+#include <stdexcept> // exceptions
+
+namespace my_stl
+{
+
+template<class T, class Container = my_deque<T>>
+class my_queue
+{
+    public:
+
+    my_queue();
+    ~my_queue() noexcept;
+    my_queue(const my_queue& other);
+    my_queue& operator=(const my_queue& other);
+
+    T& front();
+    const T& front() const;
+    T& back();
+    const T& back() const;
+
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+
+    void push(const T& value);
+    T pop();
+
+    private:
+    size_t m_size;
+};
+
+
+} // my_stl
+
+#endif /* __MY_QUEUE_HPP__ */
+
